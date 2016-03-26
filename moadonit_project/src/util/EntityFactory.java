2e083e19bd8abf8bcf38d15e0ccc927b6590d4b8
Matan <Matan@Matan-HP>
2016-03-26 12:27:07 +0300

    add jackson

--- moadonit_project/src/util/EntityFactory.java
+++ moadonit_project/src/util/EntityFactory.java
@@ -10,6 +10,8 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class EntityFactory {
 
