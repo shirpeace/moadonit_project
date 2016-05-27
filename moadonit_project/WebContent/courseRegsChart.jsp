2e81aa523beef5b1a8bdf8013ee6318eb7000208
Shir <Shir>
2016-05-27 16:30:52 +0300

    course graph finished

--- moadonit_project/WebContent/courseRegsChart.jsp
+++ moadonit_project/WebContent/courseRegsChart.jsp
@@ -1,139 +1,16 @@
 <!DOCTYPE html>
+<head>
 <meta charset="utf-8">
-<style>
 
-#chart rect{
-  fill: #4aaeea;
-}
+<link rel="stylesheet" href="css/mycss.css">
+</head>
+<body>
 
-#chart text{
-  fill: white;
-  font: 10px sans-serif;
-  text-anchor: end; 
-}
 
-.axis text{
-  font: 10px sans-serif;
-}
-
-.axis path, .axis line{
-  fill: none;
-  stroke : #fff;
-  shape-rendering: crispEdges;
-}
-
-body{
-  background: #1a1a1a;
-  color : #eaeaea;
-  padding : 10px;
-}
-
-</style>
-<h2>D3JS Simple JSON SVG barchart Horizontal with Axis</h2>
-<i>using simple JSON data</i><br/>
-taken from <a href="http://bost.ocks.org/mike/bar/3/">http://bost.ocks.org/mike/bar/3/</a> <br/><br/>
 <div id="chart"></div> 
 <script src="js/jquery.js"></script>
 <script src="js/jquery-ui.js"></script>
 <script src="//d3js.org/d3.v3.min.js"></script>
-<script>
+<script src="js/js_course_regs_chart.js"></script>
 
- $(document).ready(function(){
-
-$.ajax({
-		async: false,
-	type: 'GET',
-	datatype: 'jsonp',
-    url: "ReportsController",
-    data: {action : 'getAllCourseRegsData' },
-    success: function(data) {
-    	if(data != undefined){
-    		console.log(data);
-    		
-    	}
-    	else
-    		console.log("no data !!! ok shir");
-    },
-    error: function(e) {
-    	console.log("error");
-		
-    }
-    
-  });
-
-}); 
-var margin ={top:20, right:30, bottom:30, left:40},
-width=960-margin.left - margin.right, 
-height=500-margin.top-margin.bottom;
-
-//scale to ordinal because x axis is not numerical
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-
-//scale to numerical value by height
-var y = d3.scale.linear().range([height, 0]);
-
-var chart = d3.select("#chart")  
-          .append("svg")  //append svg element inside #chart
-          .attr("width", width+(2*margin.left)+margin.right)    //set width
-          .attr("height", height+margin.top+margin.bottom);  //set height
-var xAxis = d3.svg.axis()
-          .scale(x)
-          .orient("bottom");  //orient bottom because x-axis will appear below the bars
-
-var yAxis = d3.svg.axis()
-          .scale(y)
-          .orient("left");
-
-d3.json("ReportsController?action=getAllCourseRegsData", function(error, data){
-x.domain(data.map(function(d){ return d.activityName;}));
-y.domain([0, d3.max(data, function(d){return d.NumberOfReg;})]);
-
-var bar = chart.selectAll("g")
-                .data(data)
-              .enter()
-                .append("g")
-                .attr("transform", function(d, i){
-                  return "translate("+x(d.activityName)+", 0)";
-                });
-
-bar.append("rect")
-  .attr("y", function(d) { 
-    return y(d.NumberOfReg); 
-  })
-  .attr("x", function(d,i){
-    return x.rangeBand()-(margin.left);
-  })
-  .attr("height", function(d) { 
-    return height - y(d.NumberOfReg); 
-  })
-  .attr("width", x.rangeBand()/2);  //set width base on range on ordinal data
-
-bar.append("text")
-  .attr("x", x.rangeBand() - (margin.left/2)+6 )
-  .attr("y", function(d) { return y(d.NumberOfReg) -10; })
-  .attr("dy", ".75em")
-  .text(function(d) { return d.NumberOfReg; });
-
-chart.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate("+margin.left+","+ height+")")        
-    .call(xAxis);
-
-chart.append("g")
-    .attr("class", "y axis")
-    .attr("transform", "translate("+margin.left+",0)")
-    .call(yAxis)
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .text("NumberOfReg");
-});
-
-function type(d) {
-d.activityName = +d.activityName; // coerce to number
-return d;
-}
-
-</script>
+</body>
--- moadonit_project/src/dao/ReportsDAO.java
+++ moadonit_project/src/dao/ReportsDAO.java
@@ -6,8 +6,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -36,36 +38,80 @@ public class ReportsDAO extends AbstractDAO {
 	 */
 	private static final long serialVersionUID = 1L;
 	private String getAllCourseRegsData = "{ call ms2016.getAllCourseRegsData() }";
-	private String get = "select activityName, count(pa.pupilNum) as NumberOfReg "
-			+ " from tbl_activity a left join tbl_pupil_activities pa"
-			+ " on a.activityNum = pa.activityNum where a.activitytype = 1 group by activityName;";
-	
+	private String getGirlsRegsToCourses = "select a.activityNum, activityName, count(pa.pupilNum) as girlRegs " 
+				+ " from tbl_course c inner join tbl_activity a on c.activityNum = a.activityNum "
+				+	" left join tbl_pupil_activities pa on a.activityNum = pa.activityNum "
+				+	" left join tbl_pupil p on p.pupilNum=pa.pupilNum "
+				+   " where p.gender =2 	group by a.activityNum;";
+	private String getBoyRegsToCourses = "{ call getBoyRegsToCourses() }";
+	private String getcapacityToCourses = "select a.activityNum, activityName, pupilCapacity " 
+			+ " from tbl_course c inner join tbl_activity a on c.activityNum = a.activityNum ;";
 	
 
 	@SuppressWarnings("unchecked")
-	public JSONArray getAllCourseRegsData()
-			throws IllegalArgumentException, DAOException {
+	public JSONArray getAllCourseRegsData() throws IllegalArgumentException, DAOException {
+		
 		JSONArray CourseRegsData = new JSONArray();
-
+		Map<Integer,JSONObject> jsonMap = new HashMap<Integer,JSONObject>();
+		
 		try (PreparedStatement statement = DAOUtil
-				.prepareCallbackStatement(this.con.getConnection(), getAllCourseRegsData,
+				.prepareCallbackStatement(this.con.getConnection(), getBoyRegsToCourses,
 						new Object[] {});
-				ResultSet resultSet = statement.executeQuery();) {
+				ResultSet resultSet1 = statement.executeQuery();
+				PreparedStatement statement2 = DAOUtil
+						.prepareStatement(this.con.getConnection(), getGirlsRegsToCourses,
+								false, new Object[] {});
+						ResultSet resultSet2 = statement2.executeQuery(); 
+						PreparedStatement statement3 = DAOUtil
+						.prepareStatement(this.con.getConnection(), getcapacityToCourses,
+								false, new Object[] {});
+						ResultSet resultSet3 = statement3.executeQuery();){
 
-			while (resultSet.next()) {
+			while (resultSet3.next()) {
+				JSONObject row;
 				
-				JSONObject row =  mapCourseData(resultSet);
-				CourseRegsData.add(row);
+				row =  new JSONObject();
+				jsonMap.put(resultSet3.getInt("activityNum"), row);
+				row.put("course",resultSet3.getString("activityName"));
+				row.put("girls",0);		
+				row.put("boys",0);
+				row.put("free",resultSet3.getString("pupilCapacity"));			
+				
+			}
+			
+			while (resultSet1.next()) {
+				JSONObject row;
+				row =  jsonMap.get(resultSet1.getInt("activityNum"));
+				int last = Integer.valueOf((String) row.get("free")) - resultSet1.getInt("boyRegs");
+				row.put("boys",resultSet1.getString("boyRegs"));
+				row.put("free",""+last);	
+			}
+			
+			while (resultSet2.next()) {
+				JSONObject row;
+				row =  jsonMap.get(resultSet2.getInt("activityNum"));
+				int last = Integer.valueOf((String) row.get("free")) - resultSet2.getInt("girlRegs");
+				row.put("girls",resultSet2.getString("girlRegs"));
+				row.put("free",""+last);				
 			}
 
+			
+			
 		} catch (SQLException e) {
 			throw new DAOException(e);
 		}
-
+		
+		 Iterator<Entry<Integer, JSONObject>> it = jsonMap.entrySet().iterator();
+		    while (it.hasNext()) {
+		        @SuppressWarnings("rawtypes")
+				Map.Entry pair = (Map.Entry)it.next();
+		        CourseRegsData.add(pair.getValue());
+		        it.remove(); // avoids a ConcurrentModificationException
+		    }
 		return CourseRegsData;
 	}
 
-	public JSONArray get()
+/*	public JSONArray get()
 			throws IllegalArgumentException, DAOException {
 		JSONArray CourseRegsData = new JSONArray();
 
@@ -85,14 +131,14 @@ public class ReportsDAO extends AbstractDAO {
 		}
 
 		return CourseRegsData;
-	}
+	}*/
 	
 	@SuppressWarnings("unchecked")
 	private JSONObject mapGet(ResultSet resultSet) throws SQLException {
 		
 		JSONObject row =  new JSONObject();		
 		row.put("activityName", resultSet.getString("activityName"));
-		row.put("NumberOfReg",resultSet.getInt("NumberOfReg"));
+		row.put("רשומים",resultSet.getString("NumberOfReg"));
 
 			return row;
 	}
